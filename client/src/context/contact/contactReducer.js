62b1f1df1694cc0130d196d2845cd7122cda0fb5
Mike Schmidt <schmidt217@gmail.com>
2021-10-15 15:00:21 -0600

    add and delete contact
    Updated with mongoDB servers

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -6,6 +6,9 @@ import {
 	UPDATE_CONTACT,
 	FILTER_CONTACTS,
 	CLEAR_FILTER,
+	CONTACT_ERROR,
+	GET_CONTACTS,
+	CLEAR_CONTACTS,
 } from "../types";
 
 const contactReducer = (state, action) => {
@@ -13,14 +16,24 @@ const contactReducer = (state, action) => {
 		case ADD_CONTACT:
 			return {
 				...state,
-				contacts: [...state.contacts, action.payload],
+				contacts: [action.payload, ...state.contacts],
+				loading: false,
 			};
 		case DELETE_CONTACT:
 			return {
 				...state,
 				contacts: state.contacts.filter(
-					(contact) => contact.id !== action.payload
+					(contact) => contact._id !== action.payload
 				),
+				loading: false,
+			};
+		case CLEAR_CONTACTS:
+			return {
+				...state,
+				contacts: null,
+				filtered: null,
+				error: null,
+				current: null,
 			};
 		case SET_CURRENT:
 			return {
@@ -36,8 +49,9 @@ const contactReducer = (state, action) => {
 			return {
 				...state,
 				contacts: state.contacts.map((contact) =>
-					contact.id === action.payload.id ? action.payload : contact
+					contact._id === action.payload._id ? action.payload : contact
 				),
+				loading: false,
 			};
 		case FILTER_CONTACTS:
 			return {
@@ -50,11 +64,25 @@ const contactReducer = (state, action) => {
 						contact.phone.match(regex)
 					);
 				}),
+				loading: false,
 			};
 		case CLEAR_FILTER:
 			return {
 				...state,
 				filtered: null,
+				loading: false,
+			};
+		case CONTACT_ERROR:
+			return {
+				...state,
+				error: action.payload,
+				loading: false,
+			};
+		case GET_CONTACTS:
+			return {
+				...state,
+				contacts: action.payload,
+				loading: false,
 			};
 
 		default:
